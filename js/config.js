93c08150a6f1a15f945720dd3e676f644ddd2622
mrdivine98 <divinen36@gmail.com>
2025-11-26 17:08:06 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_3jXEZSAhR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2jnrlpbkb2v6jvljti9dptguk7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
