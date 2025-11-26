4190ccbb6f6bed41259b408b22f50890cfefab29
mrdivine98 <divinen36@gmail.com>
2025-11-26 17:34:42 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-2_3jXEZSAhR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ti5g2pojk68gs8gte096c0sma', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
